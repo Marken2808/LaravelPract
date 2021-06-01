af22538c7fd503a0b31616eec8fa5855a10823a2
Marken <27118779+Marken2808@users.noreply.github.com>
2021-06-01 21:35:37 +0100

    n

---
+++ posty/resources/views/posts/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="d-flex justify-content-center my-5">
+        <div class="p-5 bg-white rounded">
+            Post
+        </div>
+    </div>
+@endsection
