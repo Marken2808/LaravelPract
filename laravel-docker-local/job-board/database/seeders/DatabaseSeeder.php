be8bd0a18723aecb61cb58d1852ef87c53de24ee
Marken <nctuan.ityu@gmail.com>
2021-11-09 23:23:23 +0000

    test database

--- laravel-docker-local/job-board/database/seeders/DatabaseSeeder.php
+++ laravel-docker-local/job-board/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Tag;
+use App\Models\Listing;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,18 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $tags = Tag::factory(10)->create();
+
+        User::factory(20)->create()->each(function($user) use($tags) {
+            Listing::factory(rand(1,4))->create([
+                'user_id' => $user -> id
+            ])->each(function($listing) use($tags) {
+                $listing->tags()->attach($tags->random(2));
+            });
+        });
+
+        
+        
     }
 }
