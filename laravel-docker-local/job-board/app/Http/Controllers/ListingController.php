068c627bcf31499d60db2e01a1572437e8b560cd
Marken <nctuan.ityu@gmail.com>
2021-11-11 16:25:57 +0000

    implement laravel mix views

---
+++ laravel-docker-local/job-board/app/Http/Controllers/ListingController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Listing;
+use Illuminate\Http\Request;
+
+class ListingController extends Controller
+{
+    public function index(){
+        $listings = Listing::where('is_active', true)
+            ->with('tags')
+            ->latest()
+            ->get();
+        // return $listings;
+        return view('listings.index', compact('listings'));
+    }
+}
