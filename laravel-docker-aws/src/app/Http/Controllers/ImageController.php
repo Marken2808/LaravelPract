4fcd85f3106df75ef69ea2c9640bd9e22c014d41
Marken <nctuan.ityu@gmail.com>
2021-11-20 18:56:57 +0000

    upload and open public access aws s3

--- laravel-docker-aws/src/app/Http/Controllers/ImageController.php
+++ laravel-docker-aws/src/app/Http/Controllers/ImageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -14,11 +15,20 @@ class ImageController extends Controller
     public function store(Request $request)
     {
         $path = $request->file('image')->store('images', 's3');
-        return $path;
+        
+        Storage::disk('s3')->setVisibility($path, 'public');
+
+        $image = Image::create([
+            'filename' => basename($path),
+            'url' => Storage::disk('s3')->url($path)
+        ]);
+
+        return $image;
     }
 
     public function show(Image $image)
     {
-        # code...
+        return Storage::response('images/'.$image->filename);
+        // return $image->url;
     }
 }
